18478c1325d599f3b4334ee410833c815297d0c8
princisgh <matteo.princisgh@studenti.unimi.it>
2022-12-15 08:42:21 +0100

    added day15

---
+++ 2022/15/Solution.java
@@ -0,0 +1,98 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+public class Solution {
+
+    public static record Point(int x, int y) {
+    }
+
+    // basically the same as Point, defined for clarity
+    public static record Range(int start, int stop) { 
+    }
+
+    public static void main(String[] args) throws IOException {
+        String in = Files.readString(Path.of("input.txt")).replaceAll("[=|,|:]+", " ");
+        Scanner s = new Scanner(in);
+        Map<Point, Point> sensors = new HashMap<>();
+        while (s.hasNextLine()) {
+            String[] line = s.nextLine().split("[ |  ]+");
+            Point sensor = new Point(Integer.parseInt(line[3]), Integer.parseInt(line[5]));
+            Point beacon = new Point(Integer.parseInt(line[11]), Integer.parseInt(line[13]));
+            sensors.put(sensor, beacon);
+        }
+        s.close();
+
+        System.out.println("part1 : " + solve(2000000, sensors));
+
+        int height = 0;
+        long x = 0;
+        while ((x = solve2(height, sensors)) < 0)
+            height++;
+        System.out.println("part2 : " + ((x * 4000000) + height));
+
+    }
+
+
+    static int solve(int height, Map<Point, Point> sensors) {
+        List<Range> atHeight = rangesAtHeight(height, sensors);
+       
+        int count = 0;
+        // hardcoded values, should be refactored to be the min/max x coordinates reached by a signal
+        for (int i = -10000000; i < 5000000; i++) { 
+            boolean hasSignal = false;
+            for (Range rn : atHeight) {
+                if (i >= rn.start && i <= rn.stop && !hasSignal) {
+                    count++;
+                    hasSignal = true;
+                }
+            }
+        }
+        return count;
+    }
+
+    static int solve2(int height, Map<Point, Point> sensors) {
+        List<Range> atHeight = rangesAtHeight(height, sensors);
+
+        for (int i = 0; i < 4000000; i++) {
+            boolean hasSignal = false;
+            for (Range rn : atHeight) {
+                if (i >= rn.start && i <= rn.stop && !hasSignal) {
+                    hasSignal = true;
+                    i = rn.stop;
+                }
+            }
+            if (!hasSignal)
+                return i;
+        }
+        return -1;
+    }
+
+    // list of signal range for each sensor at a given height
+    public static List<Range> rangesAtHeight(int height, Map<Point, Point> sensors){
+        List<Range> atHeight = new ArrayList<>();
+        for (Point p : sensors.keySet()) {
+            Range r = rangeAt(p, sensors.get(p), height);
+            if (r != null)
+                atHeight.add(r);
+        }
+        return atHeight;
+    }
+
+    // given the height calculate the range of occupied x coordinates by sensor's
+    // signal
+    public static Range rangeAt(Point sensor, Point beacon, int line) {
+        int max_extension = ManhattanDistance(sensor, beacon);
+        int lineDist = Math.abs(line - sensor.y);
+        if (lineDist > max_extension)
+            return null;
+        int occupation = max_extension - (lineDist);
+        Range res = new Range(sensor.x - occupation, sensor.x + occupation);
+        return res;
+    }
+
+    public static int ManhattanDistance(Point a, Point b) {
+        return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+    }
+}

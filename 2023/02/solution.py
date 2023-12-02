3d5960df58518f75b87a5521d812f733e91f7e7c
matteo <matteo.princisgh@studenti.unimi.it>
2023-12-02 11:58:39 +0100

    shortened day2

--- 2023/02/solution.py
+++ 2023/02/solution.py
@@ -1,15 +1,14 @@
 import numpy as np
+import re
 
 def part2(lines:str):
     result = 0
     for line in lines:
         gameDict = {'red':0, 'green':0, 'blue':0}
-        hands = line.split('; ')
-        for hand in hands:
-            colors = hand.split(', ')
-            for color in colors:
-                num, colorStr = color.split(' ')
-                gameDict[colorStr] = max(int(num), gameDict[colorStr])
+        lst = re.split('[; |, ]+', line)
+        for i in range(0, len(lst), 2):
+            num, color = lst[i:i+2]
+            gameDict[color] = max(int(num), gameDict[color])
         result += np.prod(list(gameDict.values()))
     return result
 
@@ -17,23 +16,20 @@ def part1(lines:str):
     maxValues = {'red' : 12, 'green' : 13, 'blue' : 14}
     idSum = 0
     for i, line in enumerate(lines):
-        hands = line.split('; ')
         ok = True
-        for hand in hands:
-            colors = hand.split(', ')
-            for color in colors:
-                num, colorStr = color.split()
-                if int(num) > maxValues[colorStr]:
-                    ok = False
+        lst = re.split('[; |, ]+', line)
+        for j in range(0, len(lst), 2):
+            num, color = lst[j:j+2]
+            if int(num) > maxValues[color]:
+                ok = False
         if ok:
             idSum += i + 1
-    return  idSum
+    return idSum
 
 
 if __name__ == "__main__":
     with open("input.txt", "r") as f:
         lines = f.readlines()
     lines = [line.split(': ')[1].strip() for line in lines]
-
     print(part1(lines))
     print(part2(lines))
